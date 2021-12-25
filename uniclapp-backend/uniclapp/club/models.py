06e45edc405fd0194bd9eca76775b358bcd343e3
Görkem Ayten <gorkem.ayteen@gmail.com>
2021-12-25 03:34:01 +0300

    Database Files edited.

--- uniclapp-backend/uniclapp/club/models.py
+++ uniclapp-backend/uniclapp/club/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 from club import enums
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Club(models.Model):
