1a3fbe87277e67555b9319a9cba8feaf1ff7a7ac
Görkem Ayten <gorkem.ayteen@gmail.com>
2021-12-28 19:16:43 +0300

    Bug-fix.

--- uniclapp-backend/uniclapp/event/views.py
+++ uniclapp-backend/uniclapp/event/views.py
@@ -36,6 +36,7 @@ class EventCreateAPIView(generics.CreateAPIView):
                 if serializer.is_valid():
                     serializer.save()
                     return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class EventDestroyAPIView(generics.DestroyAPIView):
